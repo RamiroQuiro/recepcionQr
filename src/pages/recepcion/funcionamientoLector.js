ecec9f653263adbd96e1386f13c12615316864a7
ramiroquiro <rama.exe.13@gmail.com>
2023-11-07 00:52:38 -0300

    mejorando captura de camaras

--- src/pages/recepcion/funcionamientoLector.js
+++ src/pages/recepcion/funcionamientoLector.js
@@ -1,68 +1,45 @@
 import jsQR from 'jsqr' // Importamos la librería jsQR para leer códigos QR
 
-  // Referencias a los botones de prender y apagar
-  // const bottonPrender=document.getElementById('prender')
-  // const bottonApagar=document.getElementById('apagar')
-  // Referencias al stream y al video
   let streamRef;
   let videoRef;
 
   // Función para obtener los medios conectados
   const obtenerMediosConectados = async () => {
-    const getDevices = () => navigator.mediaDevices.enumerateDevices();
     try {
-      const mediaDevices = await getDevices().then((devices) => {
-        let opcionesDeCamaras = [];
-        // Recorremos los dispositivos y guardamos las cámaras
-        for (const devicesInfo of devices) {
-          if (devicesInfo.kind == "videoinput") {
-            opcionesDeCamaras.push({
-              value: devicesInfo.deviceId,
-              label: devicesInfo.label,
-            });
-          }
-        }
-      });
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      let opcionesDeCamaras = devices.filter(device => device.kind === "videoinput");
     } catch (error) {
-      console.log(error);
+      console.error("Error al obtener los medios conectados: ", error);
     }
   };
 
   // Función para obtener el video
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({
+  const getVideo = async () => {
+    try {
+      streamRef = await navigator.mediaDevices.getUserMedia({
         video: { width: 960 , height:540 },
         audio: true,
-      })
-      .then((stream) => {
-        streamRef = stream;
-        if (streamRef) {
-          videoRef = document.querySelector('video');
-          videoRef.srcObject = stream;
-          videoRef.play();
-          // Cuando el video esté cargado, iniciamos el escaneo
-          videoRef.addEventListener('loadedmetadata', () => {
-            scan();
-          });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
       });
+      videoRef = document.querySelector('video');
+      videoRef.srcObject = streamRef;
+      videoRef.play();
+      // Cuando el video esté cargado, iniciamos el escaneo
+      videoRef.addEventListener('loadedmetadata', scan);
+    } catch (error) {
+      console.error("Error al obtener el video: ", error);
+    }
   };
 
   // Función para detener el video
   const detenerVideo = () => {
-    videoRef.srcObject.getTracks().forEach(pista => {
-      pista.stop(); // Detiene cada pista del video
-    });
-    videoRef = document.querySelector('video');
-    videoRef = null; // Limpia la referencia al video
+    if (videoRef && videoRef.srcObject) {
+      videoRef.srcObject.getTracks().forEach(pista => pista.stop());
+      videoRef = null; // Limpia la referencia al video
+    }
   };
 
   // Función para escanear el código QR
-  function scan() {
+  const scan = () => {
     const canvas = document.createElement("canvas");
     canvas.width = videoRef.videoWidth;
     canvas.height = videoRef.videoHeight;
@@ -71,23 +48,12 @@ import jsQR from 'jsqr' // Importamos la librería jsQR para leer códigos QR
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const code = jsQR(imageData.data, imageData.width, imageData.height);
     if (code) {
-    //   console.log("Código QR encontrado: " + code.data);
       window.location.href = code.data;
     } else {
       setTimeout(scan, 300);
     }
   }
 
-  // Evento para iniciar el video
-  bottonPrender.addEventListener('click', () => {
-    videoRef = document.querySelector('video');
-    obtenerMediosConectados();
-    getVideo(); // Llama a la función para obtener el video
-  });
-
-  // Evento para detener el video
-  bottonApagar.addEventListener('click', () => {
-    detenerVideo(); // Llama a la función para detener el video
-    videoRef.hidden = true; // Oculta el video
-  });
-
+  // Iniciamos la obtención de medios y el video
+  obtenerMediosConectados();
+  getVideo();
