49fc2a2bd5b324472661bb48ca8671e8a25e70de
RamiroQuiro <rama.exe.13@gmail.com>
2023-11-10 10:22:54 -0300

    adaptador vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
 });
