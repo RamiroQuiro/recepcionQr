b5a59fb2013d9a6c5ea91e7a9d08b0a3fe2099a4
ramiroquiro <rama.exe.13@gmail.com>
2023-12-09 02:37:19 -0300

    trayendo data para un copmnente astro

--- src/pages/eventos/components/ListadoCredencialesCheckIn.jsx
+++ src/pages/eventos/components/ListadoCredencialesCheckIn.jsx
@@ -1,54 +1,73 @@
-import {useEffect,useState} from "react";
+import {useEffect,useState,useRef} from "react";
 
 export default function ListadoCredencialesCheckIn({ uid }) {
 
 const [dataEvento, setDataEvento] = useState({})
+const [widthContenedor, setWidthContenedor] = useState(0)
+const refContenedor = useRef();
+useEffect(() => {
+  const fetching = async () => {
+    const resCheckIn = await fetch(`http://localhost:4321/api/eventos`);
+    const respuesta = await resCheckIn.json();
+    console.log(respuesta)
+    const evento = respuesta?.eventos?.find((evento) => evento.uid == uid);
+    setDataEvento({
+      checkIn: evento.checkIn,
+      acreditaciones: evento.acreditaciones,
+    });
+  };
+
+  fetching();
+
+  if (refContenedor.current) {
+    setWidthContenedor(refContenedor.current.clientWidth);
+  }
+console.log(((dataEvento?.checkIn?.length / widthContenedor) * 100))
+console.log('whidth contenedor ',widthContenedor)
+console.log('length ',dataEvento)
+}, []);
 
 
-  useEffect(() => {
-    const fetching = async () => {
-      const resCheckIn = await fetch(`http://localhost:4321/api/eventos`);
-      const respuesta = await resCheckIn.json();
-      const evento = respuesta.eventos.find((evento) => evento.id === uid);
-      setDataEvento({
-        checkIn: evento.checkIn,
-        acreditaciones: evento.acreditaciones,
-      });
-    };
 
-    fetching();
-  }, []);
-
-
-console.log(dataEvento);
   return (
     <div className="bg-white rounded-lg w-full min-h-[50vh]  text-paleta1-gray p-4">
-      <h1>Total de Credenciales {2}</h1>
+      <h1>Total de Credenciales {dataEvento?.acreditaciones?.length || 0}</h1>
 
-      <div className="w-full h-10 my-10 bg-white/50  cursor-pointer backdrop-blur-sm  rounded-lg  border-2 border-gray-500/50 flex items-center relative">
-        <div className=" bg-gradient-to-tr from-blue-600/80 to-blue-400/80 rounded-l-md  backdrop-blur-sm w-1/3 absolute group top-0 left-0 h-full ">
+      <div
+      ref={refContenedor} 
+      className="w-full h-10 my-10 bg-white/50  cursor-pointer backdrop-blur-sm  rounded-lg  border-2 border-gray-500/50 flex items-center relative">
+        <div
+        style={{
+          width: `calc(${((dataEvento?.checkIn?.length / widthContenedor) * 100) || 0}% - 2em)`,
+        }}
+        className=" bg-gradient-to-tr from-blue-600/80 to-blue-400/80 rounded-l-md  backdrop-blur-sm w-1/3 absolute group top-0 left-0 h-full ">
           <div className="w-full h-full relative flex items-center">
             <span className="z-10 mx-auto bg-white rounded-full text-sm font-bold py-1 px-2.5 border-2 ">
               {" "}
-              {2}
+              {dataEvento?.checkIn?.length || 0}
             </span>
             <span className="absolute bottom-full text-xs font-bold opacity-0 group-hover:opacity-100 group-hover:-translate-y-2 duration-300">
               Credenciales Ingresadas
             </span>
           </div>
         </div>
-        <div className="bg-gradient-to-tr from-red-500/90 to-red-400/80 rounded-r-md backdrop-blur-sm group w-2/3 absolute top-0 right-0 h-full  border-l-2 border-white/80 ">
+        <div
+
+        style={{
+          width: `calc(${((dataEvento?.acreditaciones?.length - dataEvento?.checkIn?.length) * 10) || 0}% - 2em)`
+        }}
+        className="bg-gradient-to-tr from-red-500/90 to-red-400/80 rounded-r-md backdrop-blur-sm group w-2/3 absolute top-0 right-0 h-full  border-l-2 border-white/80 ">
         <div className="w-full h-full  text-right relative flex items-center">
             <span className="z-10 mx-auto bg-white rounded-full text-sm font-bold py-1 px-2.5 border-2 opacity-0 group-hover:opacity-100 duration-300 ">
               {" "}
-              {dataEvento?.acreditaciones?.length || 0}
+              {(dataEvento?.acreditaciones?.length - dataEvento?.checkIn?.length) || 0}
             </span>
             <span className="absolute bottom-full right-0 text-xs font-bold ">
               Credenciales No Ingresadas
             </span>
           </div>
         </div>
-        <div></div>
+        
       </div>
     </div>
   );
