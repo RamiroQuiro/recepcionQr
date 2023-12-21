0fdeb9ad6638ce8f0fac001c23275e8218e0e8f4
RamiroQuiro <rama.exe.13@gmail.com>
2023-12-21 15:32:15 -0300

    mandando mails y nodemailer

--- src/pages/api/sendmailer/router.js
+++ src/pages/api/sendmailer/router.js
@@ -8,16 +8,16 @@ export const POST=async({request})=>{
     const uidEvento=data.get('uidEvento')
     const nombreApellido=data.get("nombreApellido");
     const image = data.get("image");
+  
     // Leer la imagen como un Buffer
-    const imageBuffer = await fs.readFile(image.path);
+    const imageBuffer = await image.arrayBuffer();
 
     // Convertir el Buffer a una cadena base64
-    const base64Image = imageBuffer.toString('base64');
+    const base64Image = Buffer.from(imageBuffer).toString('base64');
 
-    console.log('base64Image ->', base64Image);
 
-    const template=getTemplate(nombreApellido,)
-// sendMailer(email,'prueba',getTemplate(nombreApellido,canvas))
+    const template=getTemplate(nombreApellido,base64Image)
+sendMailer(email,'prueba',template,base64Image)
 return new Response(JSON.stringify({
     status:200,
     message:email,
