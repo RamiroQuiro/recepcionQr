53a924a3b6abd7e454ee7c4b5266fb51f4ad54d5
RamiroQuiro <rama.exe.13@gmail.com>
2023-12-22 14:44:59 -0300

    variables de enteorno

--- src/nodemailer/nodemailer.js
+++ src/nodemailer/nodemailer.js
@@ -97,7 +97,8 @@ export const getTemplate=(name,dataURL)=>{
                        </tr>
                        
                        <tr>
-                        <td align="center" class="es-m-p0r es-m-p0l" style="Margin:0;padding-top:5px;padding-bottom:5px;padding-left:40px;padding-right:40px"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#e7812f;font-size:14px"><strong>RamiroCode </strong>| WebDeveloper.</p></td>
+                        <td align="center" class="es-m-p0r es-m-p0l" style="Margin:0;padding-top:5px;padding-bottom:5px;padding-left:40px;padding-right:40px">
+                        <a target="_black" href="https://ramiroquiroga.vercel.app" style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#e7812f;font-size:14px"><strong>RamiroCode </strong>| WebDeveloper.</a></td>
                        </tr>
                      </table></td>
                    </tr>
