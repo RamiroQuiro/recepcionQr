aee70cd9a7b72a8b37a5eaf4b7b205f82b72b2c2
RamiroQuiro <rama.exe.13@gmail.com>
2023-12-23 17:20:06 -0300

    fething de credenciales en el front

---
+++ src/pages/api/eventos/[uidEvento].js
@@ -0,0 +1,38 @@
+import path from "path";
+import fs from 'fs/promises'
+
+
+const DES = import.meta.env.URL_DESARROLLO;
+const PRODUC = import.meta.env.URL_PRODUCCION;
+const isDev = import.meta.env.DEV;
+
+// Define la ruta base dependiendo del entorno
+const basePath = !isDev  ? PRODUC : DES;
+
+export const GET = async ({ request }) => {
+    // Define la ruta del archivo
+    const filePathData = path.join(process.cwd(), basePath, "base", "base.json");
+  const url=await request.url.split('/')[5]
+  // Lee el archivo y parsea el contenido a un array
+  const dataBase = JSON.parse(await fs.readFile(filePathData, "utf8"));
+  let arrayEventos = dataBase.eventos?.filter((evento)=>evento.uid===url)?.map((element) => {
+      return {
+        name: element.nombre,
+        uid: element.uid,
+        portada: element.path,
+        nVideos: element.videos.length,
+        videos: element.videos,
+        acreditaciones: dataBase.credenciales.filter(
+          (creden) => creden.evento == element.uid
+        ),
+        checkIn:element.checkIn
+      };
+    });
+    // Agrega los nuevos datos al array
+    return new Response(
+      JSON.stringify({
+        acreditaciones: arrayEventos,
+      })
+    );
+  };
+  
--- src/pages/api/sendmailer/router.js
+++ src/pages/api/sendmailer/router.js
@@ -1,26 +1,30 @@
-import fs from 'fs/promises'
 import { getTemplate, sendMailer } from "../../../nodemailer/nodemailer";
 
 export const POST=async({request})=>{
 
-    const data = await request.formData();
-    const email=data.get("mail");
-    const uidEvento=data.get('uidEvento')
-    const nombreApellido=data.get("nombreApellido");
-    const image = data.get("image");
-  
-    // Leer la imagen como un Buffer
-    const imageBuffer = await image.arrayBuffer();
+    try {
+        const data = await request.formData();
+        const email = data.get("mail");
+        const uidEvento = data.get('uidEvento');
+        const nombreApellido = data.get("nombreApellido");
+        const image = data.get("image");
 
-    // Convertir el Buffer a una cadena base64
-    const base64Image = Buffer.from(imageBuffer).toString('base64');
+        // Leer la imagen como un Buffer
+        const imageBuffer = await image.arrayBuffer();
 
+        // Convertir el Buffer a una cadena base64
+        const base64Image = Buffer.from(imageBuffer).toString('base64');
 
-    const template=getTemplate(nombreApellido,base64Image)
-sendMailer(email,'prueba',template,base64Image)
-return new Response(JSON.stringify({
-    status:200,
-    message:email,
-    uidEvento
-}))
+        const template = getTemplate(nombreApellido, base64Image);
+        sendMailer(email, 'prueba', template, base64Image);
+        return new Response(JSON.stringify({
+            status: 200,
+            message: email,
+        }));
+    } catch (error) {
+        return new Response(JSON.stringify({
+            status: 500,
+            message: email,
+        }));
+    }
 }
