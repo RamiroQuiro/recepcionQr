efe03f0877091485aec700bc8e6821d23687a877
RamiroQuiro <rama.exe.13@gmail.com>
2024-01-16 17:33:36 -0300

    cmabiando ui del lector

--- src/pages/recepcion/receptor/componentes/Listado.jsx
+++ src/pages/recepcion/receptor/componentes/Listado.jsx
@@ -11,7 +11,6 @@ export default function Listado({uidEvento,eventoName}) {
   const serverUrl = !isDev  ? PRODUC : DES;
 
 
-  console.log(serverUrl)
   useEffect(() => {
     // Crear un nuevo objeto EventSource
     const source = new EventSource(`http://${'172.16.0.10'}:8000/event-stream?uidEvento=${uidEvento}`);
@@ -25,7 +24,7 @@ export default function Listado({uidEvento,eventoName}) {
   }, [uidEvento]);
   return (
 
-      <div className="p-10 flex mx-auto md:w-10/12 w-11/12 items-stretch justify-center rounded shadow-sm bg-white min-h-[75vh]">
+      <div className="md:p-10 p-2 flex mx-auto md:w-10/12 w-full items-stretch justify-center rounded shadow-sm bg-white min-h-full md:min-h-[75vh]">
         <ul className="flex flex-col bg-paleta1-secondary rounded-md  p-4 w-full">
        {checkIn?.map((credencial)=>(
         <CardCheckIn credencial={credencial} key={credencial.uid} eventoName={eventoName}/>
